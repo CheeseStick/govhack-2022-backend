54a7e33c580f0cac5abb2e54a59187b10e5eb163
CheeseStick <9913103+CheeseStick@users.noreply.github.com>
2022-08-20 13:01:28 +1200

    [Update] Adding django rest framework dependencies to project.

--- GovHack2022/settings.py
+++ GovHack2022/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Libraries
+    "rest_framework",
+
+    # Apps
+
 ]
 
 MIDDLEWARE = [
