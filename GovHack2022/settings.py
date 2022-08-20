d0bac55829a6deaeff3a1ce9f68148f709dbeb2f
CheeseStick <9913103+CheeseStick@users.noreply.github.com>
2022-08-20 19:24:52 +1200

    [Update] Adding CORS header for front-end.

--- GovHack2022/settings.py
+++ GovHack2022/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # Libraries
     "rest_framework",
+    "corsheaders",
 
     # Apps
     "PipeNetwork.apps.PipeNetworkConfig",
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,7 +134,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Math
+# CORS Headers
+# https://github.com/adamchainz/django-cors-headers
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+# Math
+#
 with open("GovHack2022/resources/noise_profiles.pkl", "rb") as f:
     NOISE_PROFILES = pickle.load(f)
