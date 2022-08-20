5e4e6493dde497f8a9cd7ef3f2d9ec1044443eb6
20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
2022-08-20 14:10:23 +1200

    Create serializers.py
    [ADD] Implementing Pipe, PipeGeometry and PipeWithGeometry serializers.

---
+++ PipeNetwork/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Pipe, PipeGeometry
+
+
+class PipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pipe
+        fields = '__all__'
+
+
+class PipeGeometrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PipeGeometry
+        fields = '__all__'
+
+
+class PipeWithGeometrySerializer(serializers.ModelSerializer):
+    geometries = PipeGeometrySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Pipe
+        fields = '__all__'
