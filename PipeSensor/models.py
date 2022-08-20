ce4ae4c639679abaf05ccae480ccb4a97fb35ff6
CheeseStick <9913103+CheeseStick@users.noreply.github.com>
2022-08-20 17:21:55 +1200

    [Add] Implementing SensorReport model from Pipe.
    - Generating DB migrations for PipeSensorReport model.

--- PipeSensor/models.py
+++ PipeSensor/models.py
@@ -1,3 +1,19 @@
+from datetime import datetime, timezone
+
 from django.db import models
 
-# Create your models here.
+from PipeNetwork.models import Pipe
+
+
+class PipeSensorReport(models.Model):
+    pipe = models.ForeignKey(Pipe, db_index=True, on_delete=models.CASCADE)
+    reported_at = models.DateTimeField(auto_created=True)
+
+    flow_rate = models.FloatField("Flow Rate (m^3/s)", default=0.0)
+    water_level = models.FloatField("Water Level (%)", default=0.0)
+
+    def save(self, *args, **kwargs):
+        if not hasattr(self, "id") or self.id is None:
+            self.reported_at = datetime.now(tz=timezone.utc)
+
+        super(PipeSensorReport, self).save(*args, **kwargs)
