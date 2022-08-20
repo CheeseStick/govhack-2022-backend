b9e715b2c6d43b507639eebc46c244d7fc577ca5
CheeseStick <9913103+CheeseStick@users.noreply.github.com>
2022-08-20 13:44:40 +1200

    [Add] Implementing Pipe model and PipeGeometry model.
    - Registering Pipe and PipeGeometry models to admin.
    - Generating DB migrations.
    - Registering PipeNetwork app to main project.

--- PipeNetwork/admin.py
+++ PipeNetwork/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Pipe, PipeGeometry
 
-# Register your models here.
+
+class PipeGeometryInline(admin.TabularInline):
+    model = PipeGeometry
+    fields = ("latitude", "longitude", "level", )
+
+
+@admin.register(Pipe)
+class PipeAdmin(admin.ModelAdmin):
+    fields = ("id", "asset_id", "pipe_type", "length", "district", "diameter", "material", "depth", )
+    search_fields = ("id", "pipe_type", "district", "material", )
+    list_filter = ("pipe_type", "district", "material", )
+    inlines = (PipeGeometryInline, )
