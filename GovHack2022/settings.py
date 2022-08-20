c98f6c48559fa08c2b915deca5f67d72f6773b67
Lite W Kim <litek@orionhealth.com>
2022-08-20 15:23:24 +1200

    *

--- GovHack2022/settings.py
+++ GovHack2022/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .math_utility.gen_noise_profiles import generate_noise_profiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Math
+
+NOISE_PROFILES = generate_noise_profiles(10)
